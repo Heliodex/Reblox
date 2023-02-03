d2af9f69765e4f52fecb7be074e12e5897798e5a
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-02-03 00:37:32 +0000

    Add graphql test with deno

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,22 @@
-console.log("Welcome to Deno!")
+import { serve } from "https://deno.land/std/http/server.ts"
+import { gql } from "https://deno.land/x/graphql_tag/mod.ts"
+import { GraphQLHTTP } from "https://deno.land/x/gql/mod.ts"
+import { makeExecutableSchema } from "https://esm.sh/@graphql-tools/schema"
+
+const typeDefs = gql`
+	type Query {
+		hello: String
+		goodbye: String
+	}
+`
+
+const resolvers = {
+	Query: {
+		hello: () => `Hello World!`,
+		goodbye: () => `Goodbye World!`,
+	},
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+await serve(GraphQLHTTP({ schema, graphiql: true }), { port: 3000 })
