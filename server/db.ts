eb0d381b743744442a5c59e08c2c903a136da702
Lewin Kelly <heli@odex.cf>
2023-02-04 23:53:33 +0000

    Add surrealdb to docker

--- server/db.ts
+++ server/db.ts
@@ -1,44 +1,45 @@
 import Surreal from "https://deno.land/x/surrealdb/mod.ts"
 
-const db = new Surreal("http://127.0.0.1:8000/rpc")
+const db = new Surreal("http://localhost:8000/rpc")
 
-async function main() {
-	try {
-		// Signin as a namespace, database, or root user
-		await db.signin({
-			user: "root",
-			pass: "root",
-		})
+try {
+	// Signin as a namespace, database, or root user
+	await db.signin({
+		user: "root",
+		pass: "root",
+	})
 
-		// Select a specific namespace / database
-		await db.use("test", "test")
+	// Select a specific namespace / database
+	await db.use("test", "test")
 
-		// Create a new person with a random id
-		let created = await db.create("person", {
-			title: "Founder & CEO",
-			name: {
-				first: "Tobie",
-				last: "Morgan Hitchcock",
-			},
-			marketing: true,
-			identifier: Math.random().toString(36).substr(2, 10),
-		})
+	// Create a new person with a random id
+	const created = await db.create("person", {
+		title: "Founder & CEO",
+		name: {
+			first: "Tobie",
+			last: "Morgan Hitchcock",
+		},
+		marketing: true,
+		identifier: Math.random().toString(36).substr(2, 10),
+	})
 
-		// Update a person record with a specific id
-		let updated = await db.change("person:jaime", {
-			marketing: true,
-		})
+	// Update a person record with a specific id
+	const updated = await db.change("person:jaime", {
+		marketing: true,
+	})
 
-		// Select all people records
-		let people = await db.select("person")
+	// Select all people records
+	const people = await db.select("person")
 
-		// Perform a custom advanced query
-		let groups = await db.query("SELECT marketing, count() FROM type::table($tb) GROUP BY marketing", {
-			tb: "person",
-		})
-	} catch (e) {
-		console.error("ERROR", e)
-	}
+	// Perform a custom advanced query
+	const groups = await db.query("SELECT marketing, count() FROM type::table($tb) GROUP BY marketing", {
+		tb: "person",
+	})
+
+	console.log("created\n", created)
+	console.log("updated\n", updated)
+	console.log("people\n", people)
+	console.log("groups\n", groups)
+} catch (e) {
+	console.error("ERROR", e)
 }
-
-main()
