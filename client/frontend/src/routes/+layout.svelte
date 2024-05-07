2e68d409d8e4d41d4cda4a54c73b8cf92e1dd553
Lewin Kelly <heli@odex.cf>
2024-05-07 04:45:26 +0100

    Initialise Wails app with SvelteKit for client

---
+++ client/frontend/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import "uno.css"
+	import "../global.styl"
+
+	const { children } = $props()
+</script>
+
+{@render children()}
---
+++ client/frontend/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Greet } from "$lib/wailsjs/go/main/App"
+
+	let name = $state("")
+	let text = $state("Testing")
+
+	async function sayHi() {
+		text = await Greet(name)
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
+	to read the documentation
+</p>
+
+<p>{text}</p>
+
+<input type="text" bind:value={name} placeholder="Enter your name" />
+<button onclick={sayHi}>Greet</button>
